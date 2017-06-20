8d2820a7f8ab3c9cecf6b8f5ac6c2785a9d36f29
Ludovic Claude <ludovic.claude54@gmail.com>
2017-06-20 17:13:14 +0200

    Ignore light version of the scripts

--- regenerate-repos.sh
+++ regenerate-repos.sh
@@ -4,5 +4,5 @@ curl -H "Authorization: bearer $GITHUB_TOKEN" -X POST \
   -d "{\"query\": \"query { organization(login: \\\"HBPMedical\\\") {repositories(last: 100) {nodes {name }}}}\"}" \
   https://api.github.com/graphql | \
     jq ".data.organization.repositories.nodes[].name" | \
-    grep -i -v "\"(HBPSP8Repo.github.io|Study-WebApps|WebMIPMap_old)\"" | \
+    grep -i -v "\"(HBPSP8Repo.github.io|Study-WebApps|WebMIPMap_old|mri-preprocessing-pipeline-light)\"" | \
     sort
